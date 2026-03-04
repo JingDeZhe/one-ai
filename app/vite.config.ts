c61df48bf68d3ab1df8b8492ff92bbebc238bf31
wz <wz@none.com>
2026-03-04 17:45:29 +0800

    vue router 5文件路由适配

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite'
+import VueRouter from 'vue-router/vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [VueRouter(), vue()],
   base: '/app/',
   build: {
     outDir: '../public/app',
     emptyOutDir: true,
     sourcemap: false,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   server: {
     port: 5173,
